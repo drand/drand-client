8319ddeac65061322d590864f6986c1c0e0a846c
Patrick McClurg <patrick.mcclurg@protocol.ai>
2022-06-16 14:30:22 +0200

    Verification of beacons using noble/bls12-381
    - removed go wasm bindings
    - added some recommended limitations on engines and browsers in the
      package.json
    - updated standard linter to recognise `BigInt`
    - added an action for fixing linting issues
    - removed go build steps

--- optimizing.test.js
+++ optimizing.test.js
@@ -10,7 +10,7 @@ global.AbortController = AbortController
 
 const TESTNET_CHAIN_HASH = '84b2234fb34e835dccd048255d7ad3194b81af7d978c3bf157e3469592ae4e02'
 const TESTNET_URLS = [
-  'http://pl-us.testnet.drand.sh',
+  'http://pl-us.testnet.drand.sh'
 ]
 
 test('should get randomness from working client', async t => {
