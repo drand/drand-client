92c51ce9025cea0ec59733f8dac10726de89509d
Cameron McDonald <59149640+very-lame-username@users.noreply.github.com>
2022-08-20 22:44:29 +1000

    Add extra tests for `roundTime`

--- test/chain.test.ts
+++ test/chain.test.ts
@@ -23,3 +23,19 @@ test('should get time 0 for first round', () => {
 test('should get time 1000 for second round', () => {
   expect(Chain.roundTime(2, 0, 1)).toBe(1000)
 })
+
+test('should get time of genesis (in ms) for first round', () => {
+  expect(Chain.roundTime(1, 1595431050, 1)).toBe(1595431050000)
+})
+
+test('should get time genesis + 1000 for second round', () => {
+  expect(Chain.roundTime(2, 1595431050, 1)).toBe(1595431051000)
+})
+
+test('should work with different period as well', () => {
+  expect(Chain.roundTime(2, 1595431050, 30)).toBe(1595431080000)
+})
+
+test('and should work with a larger round number', () => {
+  expect(Chain.roundTime(2185561, 1595431050, 30)).toBe(1660997850000)
+})
