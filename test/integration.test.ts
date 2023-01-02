6368e38fd47efeec8f4b3a25a4ccfb99d23bab5b
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-01-02 14:38:10 +0100

    fixed package.json link to main
    added an npmignore for things that we don't want in the finally lib
    updated the tsconfig to emit the types into the build dir
    also added some integration tests

---
+++ test/integration.test.ts
@@ -0,0 +1,16 @@
+import {fetchBeacon, fetchBeaconByTime, HttpCachingChain, HttpChainClient} from '../lib';
+import 'jest-fetch-mock'
+
+describe('randomness client', () => {
+    const chain = new HttpCachingChain('https://api.drand.sh')
+    const client = new HttpChainClient(chain)
+
+    it('can consume randomness', async () => {
+        const beacon = await fetchBeaconByTime(client, Date.now())
+        expect(beacon.round).toBeGreaterThan(0)
+    })
+    it('can consume some round', async () => {
+        const beacon = await fetchBeacon(client, 2568884)
+        expect(beacon.round).toBeGreaterThan(0)
+    })
+})
