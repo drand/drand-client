c7833443800221b413863536294bcdd12dd1a4da
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-02-09 09:42:13 +0100

    propagate a default user agent everywhere
    enable use of a custom user agent with the HttpChainClient and not just
    HttpChain

--- lib/http-chain-client.ts
+++ lib/http-chain-client.ts
@@ -1,19 +1,22 @@
 import {Chain, ChainClient, ChainOptions, defaultChainOptions, RandomnessBeacon} from './index'
-import {jsonOrError} from './util'
+import {defaultHttpOptions, HttpOptions, jsonOrError} from './util'
 
 class HttpChainClient implements ChainClient {
 
-    constructor(private someChain: Chain, public options: ChainOptions = defaultChainOptions) {
+    constructor(
+        private someChain: Chain,
+        public options: ChainOptions = defaultChainOptions,
+        public httpOptions: HttpOptions = defaultHttpOptions) {
     }
 
     async get(roundNumber: number): Promise<RandomnessBeacon> {
         const url = withCachingParams(`${this.someChain.baseUrl}/public/${roundNumber}`, this.options)
-        return await jsonOrError(url)
+        return await jsonOrError(url, this.httpOptions)
     }
 
     async latest(): Promise<RandomnessBeacon> {
         const url = withCachingParams(`${this.someChain.baseUrl}/public/latest`, this.options)
-        return await jsonOrError(url)
+        return await jsonOrError(url, this.httpOptions)
     }
 
     chain(): Chain {
