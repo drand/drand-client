615d4310dc711260630095fd9b5a28dcde016ff5
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-03-14 09:54:29 +0100

    changed an error message

--- lib/beacon-verification.ts
+++ lib/beacon-verification.ts
@@ -46,7 +46,7 @@ async function verifyBeacon(chainInfo: ChainInfo, beacon: RandomnessBeacon, expe
         return verifySigOnG1(beacon.signature, await unchainedBeaconMessage(beacon), publicKey, 'BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_NUL_')
     }
 
-    console.error(`Beacon type ${chainInfo.schemeID} was not supported`)
+    console.error(`Beacon type ${chainInfo.schemeID} was not supported or the beacon was not of the purported type`)
     return false
 
 }
